0291db4a3588233694aa5a2dccd276d5e5411307
Carlo Bernaschina <b3rn4@hotmail.it>
2014-11-29 00:04:06 +0100

    First implementation

---
+++ index.js
@@ -0,0 +1,11 @@
+/*jslint node: true, nomen: true */
+/**
+* Developed By Carlo Bernaschina (GitHub - B3rn475)
+* www.bernaschina.com
+*
+* Distributed under the MIT Licence
+*/
+"use strict";
+
+exports.validator = require('./lib/validator.js').validator;
+exports.field = require('./lib/field.js').field;
